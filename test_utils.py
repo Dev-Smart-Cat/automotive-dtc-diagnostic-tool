60e1ff0061a8cb8204d722d5f5cb62501e3b6f6f
Flagship-One-Inc <weverson.barbieri@gmail.com>
2026-07-22 07:25:08 -0300

    add test for insert_dtc and dtc_exist functions

--- test_utils.py
+++ test_utils.py
@@ -1,6 +1,7 @@
 import os
 import pytest
-from utils import automaker_db_tables_names_dict, query_descriptions, db_connection
+import psycopg2 as pg
+from utils import automaker_db_tables_names_dict, query_descriptions, db_connection, dtc_exists, insert_dtc
 
 
 # Test 1: dictionary structure
@@ -41,3 +42,55 @@ def test_db_connection():
     conn = db_connection()      # Attempt open a Postgres connection
     assert conn is not None     # asserting object exists
     conn.close()                # Close the connection after testing
+
+
+# Fixture: creates a temporary test table before the test
+# the table is dropped after the test, keeping the DB cleaned.
+@pytest.fixture
+def test_table():
+    # Create an object connection and excecute object  
+    conn = db_connection()
+    cur = conn.cursor()
+
+    # Create a temporary table representing the automaker_dtc table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS test_dtcs (
+                automaker TEXT,
+                code TEXT,
+                description TEXT
+        )
+    """)
+
+    conn.commit()       # Commit the query to create the table
+
+    cur.execute("DROP TABLE IF EXISTS test_dtcs")
+    conn.commit()       # Commit drop table command
+    # Close dp connection
+    cur.close()
+    conn.close()
+
+
+# Test 6: insert a DTC and confirm it exists
+def test_insert_and_exists(test_table):
+    if not os.getenv("HOST_NAME"):
+        pytest.skip("No DB credentials available - skip DB test")
+
+    # Insert a new DTC to the test_table
+    inserted = insert_dtc("Ford", test_table, "P0301", "Misfire on Cylinder #1")
+    assert inserted is True      # must return true when insertion succeeds
+
+    # Confirm the inserted DTC now exists in the table
+    dtc_exists = dtc_exists(test_table, "P0301")
+    assert dtc_exists is True    # must return true if the DTC is found
+
+
+# Test 7: inserting the same DTC twice must be blocked
+def test_insert_duplicate_is_blocked(test_table):
+    if not os.getenv("HOST_NAME"):
+        pytest.skip("No DB credentials available - skip DB test")
+
+    insert_dtc("Ford", test_table, "P0301", "Misfire on Cylinder #1")
+
+    # Second insert of the same code must return False, indicating it was not inserted
+    duplicate = insert_dtc("Ford", test_table, "P0301", "Misfire on Cylinder #1")
+    assert duplicate is False       # must return False when the DTC was not inserted because it already exists
